a547dbcb47ce5745bb25002606f10c3be02c5c13
Alexandre Yembo <alexandreyembo@gmail.com>
2020-03-05 15:20:17 +0000

    add event emitter on Angular for bookstore

--- angular-8/bookstore/bookstore/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-8/bookstore/bookstore/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe} from '../recipe.model';
 
 @Component({
@@ -7,11 +7,13 @@ import { Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeSelectedEvent = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('Test', 'Test recipe', 'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2019/02/Cheesy-mince-pasta-bake.jpg'),
-    new Recipe('Test', 'Test recipe', 'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2019/02/Cheesy-mince-pasta-bake.jpg'),
-    new Recipe('Test', 'Test recipe', 'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2019/02/Cheesy-mince-pasta-bake.jpg'),
-    new Recipe('Test', 'Test recipe', 'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2019/02/Cheesy-mince-pasta-bake.jpg')
+    new Recipe('Test 1', 'Test recipe 1', 'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2019/02/Cheesy-mince-pasta-bake.jpg'),
+    new Recipe('Test 2', 'Test recipe 2', 'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2019/02/Cheesy-mince-pasta-bake.jpg'),
+    new Recipe('Test 3', 'Test recipe 3', 'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2019/02/Cheesy-mince-pasta-bake.jpg'),
+    new Recipe('Test 4', 'Test recipe 4', 'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2019/02/Cheesy-mince-pasta-bake.jpg')
   ];
 
   constructor() { }
@@ -19,4 +21,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe){
+    this.recipeSelectedEvent.emit(recipe);
+  }
 }
